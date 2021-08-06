e3a6a3ef9e90d627a7dd421ad1381127e7a92bd8
Ivan Markovic <ivanmdmarkovic@gmail.com>
2021-08-07 00:20:35 +0200

    Basic CRUD finished

--- blog/main.py
+++ blog/main.py
@@ -18,12 +18,6 @@ def get_db():
 	finally:
 		db.close()
 
-@app.get('/articles')
-def get_articles(db: Session = Depends(get_db)):
-    articles = db.query(models.Article).all()
-    return articles
-
-
 @app.post('/articles')
 def create_article(request: schemas.Article, db: Session = Depends(get_db)):
     new_article = models.Article(title=request.title, body=request.body)
@@ -31,5 +25,29 @@ def create_article(request: schemas.Article, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_article)
     return new_article
+
+@app.get('/articles')
+def get_articles(db: Session = Depends(get_db)):
+    articles = db.query(models.Article).all()
+    return articles
+
+@app.get('/articles/{id}')
+def get_articles(id:int, db: Session = Depends(get_db)):
+    article = db.query(models.Article).filter(models.Article.id == id).first()
+    return article
+
+@app.put('/articles/{id}')
+def put_article(id:int, request: schemas.Article, db: Session = Depends(get_db)):
+    article = db.query(models.Article).filter(models.Article.id == id)
+    article.update(request.dict(), synchronize_session=False)
+    db.commit()
+    return article.first()
+
+@app.delete('/articles/{id}')
+def destroy(id, db: Session = Depends(get_db)):
+	article = db.query(models.Article).filter(models.Article.id == id).delete(synchronize_session=False)
+	db.commit()
+	return {'deleted': True}
+
 
 
